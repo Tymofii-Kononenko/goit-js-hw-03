00ed77632eb5c4136a765364e391f3e95a498075
Tymofii Kononenko <timajw95@gmail.com>
2024-10-04 21:59:17 +0200

    task-1 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,11 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = (quantity * pricePerDroid);
-    const value = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    return (value);
+function slugify(title) {
+    const lowText = title.toLowerCase();
+    const splitText = lowText.split(" ");
+    const joinText = splitText.join("-");
+    return joinText;
 }
 
-makeTransaction();
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
